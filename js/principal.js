60783c9f0404e55651d9674fbc405313de4d6a7b
guilhermefq <gfantuci@gmail.com>
2017-11-09 14:27:15 -0300

    Loop de calculo

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,45 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Karoline Nutricionista";
 
+var pacientes = document.querySelectorAll(".paciente");
 
+for(var i=0;i < pacientes.length; i++) {
+    //console.log(pacientes[i].textContent);
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    pesoEhValido = true;
+    alturaEhValida = true;
+    
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+    }
+    
+    if (altura <= 0 || altura >= 3.00 ){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+    }
+    
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }   
+
+
+}
+
+/*
 var trPaciente = document.querySelector('#primeiro-paciente');
 
 var tdPeso = trPaciente.querySelector('.info-peso');
@@ -32,6 +70,6 @@ if (pesoEhValido && alturaEhValida) {
     tdImc.textContent = imc;
 }
 
-
+*/
 
 
