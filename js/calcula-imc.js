69113cd9051d33d31e195942c6eda85b2e867e39
guilhermefq <gfantuci@gmail.com>
2017-11-10 10:43:14 -0300

    Conclusao aula 5

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -20,11 +20,10 @@ for(var i=0;i < pacientes.length; i++) {
     alturaEhValida = true;
     
     if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido");
         tdImc.textContent = "Peso inválido!";
         pesoEhValido = false;
         //paciente.style.color = "red";
-        paciente//.style.backgroundColor = "lightcoral";
+        //paciente.style.backgroundColor = "lightcoral";
         paciente.classList.add("paciente-invalido");
     }
     
@@ -38,45 +37,17 @@ for(var i=0;i < pacientes.length; i++) {
     }
     
     if (pesoEhValido && alturaEhValida) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }   
 }
 
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);    
+    return imc.toFixed(2) ;
+}
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
-
-    var form = document.querySelector("#form-adiciona");
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-    
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);    
-});
 
 
 /*
