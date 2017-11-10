41f03fd36b2626dc47c53a510148c075adfc3fb3
guilhermefq <gfantuci@gmail.com>
2017-11-10 17:28:31 -0300

    Adicionado Filtro Peso

--- js/filtra.js
+++ js/filtra.js
@@ -1,6 +1,6 @@
-var campoFiltro = document.querySelector("#filtrar-tabela");
+var campoFiltroNome = document.querySelector("#filtrar-nome");
 
-campoFiltro.addEventListener("input", function() {
+campoFiltroNome.addEventListener("input", function() {
     var pacientes = document.querySelectorAll(".paciente");
 
     if (this.value.length > 0) {
@@ -13,6 +13,36 @@ campoFiltro.addEventListener("input", function() {
             var expressao = new RegExp(this.value, "i");
             
             if(expressao.test(nome)){
+                paciente.classList.remove("invisivel");    
+            } else {
+                paciente.classList.add("invisivel");
+            }
+            
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+
+var campoFiltroPeso = document.querySelector("#filtrar-peso");
+
+campoFiltroPeso.addEventListener("input", function() {
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (campoFiltroPeso.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];      
+            
+            var tdPeso = paciente.querySelector(".info-peso");
+            var peso = tdPeso.textContent;
+
+            var expressao = new RegExp(campoFiltroPeso.value, "i");
+            
+            if(expressao.test(peso)){
                 paciente.classList.remove("invisivel");    
             } else {
                 paciente.classList.add("invisivel");
